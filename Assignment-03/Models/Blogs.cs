0ee5e77e1430f6efa261c3214eca891cd8669132
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-02 15:53:23 +0530

    Add project files.

---
+++ Assignment-03/Models/Blogs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_03.Models
+{
+    [Table("Blog")]
+    public class Blogs
+    {
+        [Key]
+        public int BId { get; set; }
+        public string BTitle { get; set; }
+        public int BContent { get; set; }
+
+        public DateTime PDate { get; set; }
+        public string BAuthor { get; set; }
+    }
+}
